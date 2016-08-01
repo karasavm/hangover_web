884abddc2c2209c6e75618ef79f2b357d3cb29d3
karasavm <karasavm@gmail.com>
2016-08-01 18:43:32 +0300

    new purchase item style. getr rid of polls

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -16,73 +16,148 @@ function PurchaseListCtrl($scope, Purchase) {
 	}
 }
 
-function PurchaseItemCtrl($scope, $routeParams, $location, socket, Purchase) {	
+function PurchaseItemCtrl($scope, $routeParams, $location, Purchase, Member) {	
 	
 	$scope.purchase = Purchase.get({purchaseId: $routeParams.purchaseId});
 	
-
+	// console.log($scope.purchase)
 
 	$scope.updatePurchase = function(){
 		
-		purchase = new Purchase($scope.purchase)
-		
-		Purchase.update({purchaseId: $routeParams.purchaseId}, $scope.purchase, function(p, resp){
-			if (!p.error){
-				$location.path('purchases');		
+		purchase = $scope.purchase
+		console.log(purchase)
+		if(purchase.title.length > 0) {
+		// 	// Loop through the choices, make sure at least two 
+			if (purchase.payments.length >= 2){
+
+				zeroPayments = 0
+				for (var i=0; i< purchase.payments.length;i++){
+					if ((purchase.payments[i].amount == 0) || (purchase.payments[i].amount=='') || (purchase.payments[i].amount==null)){
+						purchase.payments[i].amount = 0
+						zeroPayments += 1;
+					}
+				}
+
+				if (zeroPayments<purchase.payments.length){
+
+					var newPurchase = new Purchase(purchase);
+					Purchase.update({purchaseId: $routeParams.purchaseId}, $scope.purchase, function(p, resp){
+						if (!p.error){
+							$location.path('purchases');		
+						} else {
+							alert("Could not Update purchase.")
+						}
+					})
+
+				}else{
+					for (var i=0; i< purchase.payments.length;i++){
+						if (purchase.payments[i].amount == 0){
+							purchase.payments[i].amount = ''
+						}
+					}
+					alert('You must enter at least one non zero amount.');
+				}
 			} else {
-				alert("Unable to Update purchase.")
+				alert('You must enter at least two people.');
+			}
+		} else {
+			alert('You must enter a title.');
+		}
+
+	};
+
+	$scope.addRestMembers = function(){
+		members = Member.query(function(data){
+
+			for (var i=0; i<members.length;i++){
+				found = false
+				for (var j=0;j<$scope.purchase.payments.length;j++){
+					if (members[i].name == $scope.purchase.payments[j].name){
+						found = true
+						break
+					}
+				}
+				if (!found){
+					$scope.purchase.payments.push({name: members[i].name, amount: ''})
+				}
 			}
 		})
-	};
+	}
 
-	$scope.addChoice = function(scope) {
-		$scope.purchase.payments.push({ name: '', amount: 0 });
-	};
+	$scope.removePayment = function(name){
+		$scope.purchase.payments = $scope.purchase.payments.filter(function(v){return v.name!=name})
+	}
+	
 }
 
-function PurchaseNewCtrl($scope, $location, Purchase) {
+function PurchaseNewCtrl($scope, $location, Purchase, Member) {
 	// Define an empty poll model object
+	
+	var initialPayments = function(){
+		var payments = [];
+		members = Member.query(function(data){
+			for (var i=0; i < members.length;i++){
+				payments.push({name: members[i].name, amount: ''})
+			}		
+		})
+		return payments;
+	}
+	
 	$scope.purchase = {
 		title: '',
-		payments: [ { name: '', amount: 0 }, { name: '', amount: 0 }, { name: '', amount: 0 }, { name: '', amount: 0 }]
+		payments: initialPayments()
 	};
 	
+	$scope.resetPayments = function(){
+		$scope.purchase.payments = initialPayments()
+	}
 
+	$scope.removePayment = function(name){
+		$scope.purchase.payments = $scope.purchase.payments.filter(function(v){return v.name!=name})
+	}
 	// Method to add an additional choice option
 	$scope.addChoice = function(scope) {
 		$scope.purchase.payments.push({ name: '', amount: 0 });
 	};
 	
 	// Validate and save the new poll to the database
-	$scope.createPoll = function() {
+	$scope.createPurchase = function() {
 		var purchase = $scope.purchase;
 		
 		// Check that a question was provided
+
 		if(purchase.title.length > 0) {
-			var paymentCount = 0;
-			
-			// Loop through the choices, make sure at least two provided
-			for(var i = 0, ln = purchase.payments.length; i < ln; i++) {
-				var payment = purchase.payments[i];
-				
-				if(payment.name.length > 0) {
-					paymentCount++
-				}
-			}
-		
-			if(paymentCount > 1) {
-				// Create a new poll from the model
-				var newPurchase = new Purchase(purchase);
-				
-				// Call API to save poll to the database
-				newPurchase.$save(function(p, resp) {
-					if(!p.error) {
-						// If there is no error, redirect to the main view
-						$location.path('purchase');
-					} else {
-						alert('Could not create purchase');
+		// 	// Loop through the choices, make sure at least two 
+			if (purchase.payments.length >= 2){
+
+				zeroPayments = 0
+				for (var i=0; i< purchase.payments.length;i++){
+					if ((purchase.payments[i].amount == 0) || (purchase.payments[i].amount=='')|| (purchase.payments[i].amount==null)){
+						purchase.payments[i].amount = 0
+						zeroPayments += 1;
 					}
-				});
+				}
+
+				if (zeroPayments<purchase.payments.length){
+
+					var newPurchase = new Purchase(purchase);
+					newPurchase.$save(function(p, resp) {
+						if(!p.error) {
+							// If there is no error, redirect to the main view
+							$location.path('purchase');
+						} else {
+							alert('Could not create purchase');
+						}
+					});
+
+				}else{
+					for (var i=0; i< purchase.payments.length;i++){
+						if (purchase.payments[i].amount == 0){
+							purchase.payments[i].amount = ''
+						}
+					}
+					alert('You must enter at least one non zero amount.');
+				}
 			} else {
 				alert('You must enter at least two people.');
 			}
@@ -90,8 +165,8 @@ function PurchaseNewCtrl($scope, $location, Purchase) {
 			alert('You must enter a title.');
 		}
 	};
-}
 
+}
 function MemberListCtrl($scope, $location, Member){
 
 	$scope.members = Member.query();
